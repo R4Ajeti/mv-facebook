27703091f9411245a938baeb4f5d5c5f11420112
Alexander <shvetcov2000@gmail.com>
2019-10-31 18:27:10 +0200

    update data arch schema: add status to Friend, image_link to User & Comment table

--- docs/facebook-data-arch.sql
+++ docs/facebook-data-arch.sql
@@ -1,29 +1,45 @@
-CREATE TABLE "Post" (
- "ID" int,
- "User_ID" int,
- "Title" varchar(15),
- "Description" varchar(500),
- PRIMARY KEY ("ID")
+CREATE TABLE `Post` (
+  `ID` int,
+  `User_ID` int,
+  `Title` varchar(15),
+  `Description` varchar(500),
+  PRIMARY KEY (`ID`),
+  KEY `FK` (`User_ID`)
 );
-CREATE INDEX "FK" ON  "Post" ("User_ID");
-CREATE TABLE "Friend" (
- "ID" int,
- "User_ID" int,
- "Friend_ID" int,
- PRIMARY KEY ("ID")
+
+CREATE TABLE `Friend` (
+  `ID` int,
+  `User_ID` int,
+  `Friend_ID` int,
+  `Status` varchar(10),
+  PRIMARY KEY (`ID`),
+  KEY `FK` (`User_ID`, `Friend_ID`)
 );
-CREATE INDEX "FK" ON  "Friend" ("User_ID", "Friend_ID");
-CREATE TABLE "User" (
- "ID" int,
- "Username" varchar(10),
- "Email" varchar(25),
- "Password" varchar(25),
- PRIMARY KEY ("ID")
+
+CREATE TABLE `User` (
+  `ID` int,
+  `Username` varchar(10),
+  `Email` varchar(25),
+  `Password` varchar(25),
+  `ImageLink` varchar(100),
+  PRIMARY KEY (`ID`)
 );
-CREATE TABLE "Like" (
- "ID" int,
- "User_ID" int,
- "Post_ID" int,
- PRIMARY KEY ("ID")
+
+CREATE TABLE `Like` (
+  `ID` int,
+  `User_ID` int,
+  `Post_ID` int,
+  PRIMARY KEY (`ID`),
+  KEY `FK` (`User_ID`, `Post_ID`)
 );
-CREATE INDEX "FK" ON  "Like" ("User_ID", "Post_ID");
+
+CREATE TABLE `Comment` (
+  `ID` int,
+  `User_ID` int,
+  `Post_ID` int,
+  `Text` varchar(200),
+  PRIMARY KEY (`ID`),
+  KEY `FK` (`User_ID`, `Post_ID`)
+);
+
+
