c545a4851aaeade1d0d820dda737bdcc1e4ad46a
Alexander <shvetcov2000@gmail.com>
2019-11-04 20:18:43 +0200

    add app layout & signin & login views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,33 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    # @events = @user.created_events
+    # @att_events = @user.attended_events
+
+    # @prev_events = @user.attended_events.previous_events.all
+    # @upcoming_events = @user.attended_events.upcoming_events.all
+  end
+
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = 'Welcome to the Event App!'
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
-  def show
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,37 @@
+# frozen_string_literal: true
+
+# https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  get 'comments/new'
-  get 'comments/create'
-  get 'comments/show'
-  get 'friends/new'
-  get 'friends/create'
-  get 'likes/new'
-  get 'likes/create'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
-  get 'sessions/new'
-  get 'sessions/create'
+  # root
+  root 'posts#index' # Show all events
+
+  # signup
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  # login
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  resources :sessions
+
+  # users
+  resources :users
+
+  # posts
+  get '/posts', to: 'posts#index'
+  get '/posts/new', to: 'posts#new'
+  post '/posts', to: 'posts#create'
+  resources :posts
+
+  # likes
+  resources :users
+
+  # comments
+  resources :users
+
+  # friends
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
