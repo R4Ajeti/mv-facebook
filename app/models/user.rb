633bfa5a9158dbc9e8370f307d678b8ee32dc132
Rinor Ajeti <r4ajeti@gmail.com>
2019-11-07 22:41:23 +0100

    FIXED - Rubocop: Offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,26 +16,26 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :user_id
   has_many :commented_posts, through: :comments, source: :post
   has_many :friendships
-  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
   def friends
-    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
-    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
+    friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
+    friends_array.concat(inverse_friendships.map { |friendship| friendship.user if friendship.confirmed })
     friends_array.compact
   end
 
   # Users who have yet to confirme friend requests
-   def pending_friends
-    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+  def pending_friends
+    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
   end
 
   # Users who have requested to be friends
   def friend_requests
-    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+    inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
   end
-  
+
   def confirm_friend(user)
-    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship = inverse_friendships.find { |f| f.user == user }
     friendship.confirmed = true
     friendship.save
   end
