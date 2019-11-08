8a6e8439320809e2c7da958673304cf28325432d
Alexander <shvetcov2000@gmail.com>
2019-11-09 00:12:19 +0200

    clean up

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,5 +5,4 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
   validates :user_id, presence: true
   validates :friend_id, presence: true
-  # validates :confirmed, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,32 +20,4 @@ class User < ApplicationRecord
   has_many :received_friendships, foreign_key: :friend_id, class_name: 'Friendship'
   has_many :requested_friends, through: :requested_friendships, source: :friend
   has_many :received_friends, through: :received_friendships, source: :user
-
-  # has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
-
-  # def friends
-  # friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
-  # friends_array.concat(inverse_friendships.map { |friendship| friendship.user if friendship.confirmed })
-  # friends_array.compact
-  # end
-
-  # # Users who have yet to confirme friend requests
-  # def pending_friends
-  # friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
-  # end
-
-  # # Users who have requested to be friends
-  # def friend_requests
-  # inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
-  # end
-
-  # def confirm_friend(user)
-  # friendship = inverse_friendships.find { |f| f.user == user }
-  # friendship.confirmed = true
-  # friendship.save
-  # end
-
-  # def friend?(user)
-  # friends.include?(user)
-  # end
 end
