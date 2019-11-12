97288cadfa2f186197234d6fc96971d8f4bb0ef6
Rinor Ajeti <r4ajeti@gmail.com>
2019-11-12 18:27:50 +0100

    UPDATED: Stickler offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,11 +34,11 @@ class User < ApplicationRecord
     end
   end
 
-  #def self.new_with_session(params, session)
+  # def self.new_with_session(params, session)
   #  super.tap do |user|
   #    if data == session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
   #      user.email = data['email'] if user.email.blank?
   #    end
   #  end
-  #end
+  # end
 end
