0e94834faa8c49edd60ea40be245eaeb08593ad1
Alexander <shvetcov2000@gmail.com>
2019-11-12 20:49:38 +0200

    add feature specs for Sign in with Facebook

--- spec/features/visit_login_spec.rb
+++ spec/features/visit_login_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe 'Visit login page', type: :feature do
     expect(page).to have_content('Password')
     expect(page).to have_content('Remember me')
   end
+  scenario 'Login with Facebook' do
+    visit new_user_session_path
+    expect(page).to have_content('Sign in with Facebook')
+  end
 end
